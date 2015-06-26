bdd5dd6f7d6068e312ed2ca876aead09010cd1e2
Alina Okhremenko <slykaramba@gmail.com>
2015-06-27 00:50:28 +0300

    LinkedList without Enumerator and Context

--- AOHomeTask5/AOHomeTask5/Source/AOLinkedList.c
+++ AOHomeTask5/AOHomeTask5/Source/AOLinkedList.c
@@ -7,3 +7,188 @@
 //
 
 #include "AOLinkedList.h"
+#include "AOLinkedListNode.h"
+
+static
+void AOLinkedListSetHead(AOLinkedList *list, AOLinkedListNode *head);
+
+static
+AOLinkedListNode *AOLinkedListGetHead(AOLinkedList *list);
+
+
+void __AOLinkedListDeallocate(AOLinkedList *object) {
+
+    AOLinkedListSetHead(object, NULL);
+    
+
+    __AOObjectDeallocate(object);
+}
+
+AOLinkedList *AOLinkedListCreateWithObject(void *object) {
+    
+    AOLinkedList *newLinkedList = AOObjectCreateOfType(AOLinkedList);
+    AOLinkedListNode *newLinkedListNode = AOLinkedListNodeCreateWithData(object);
+    AOLinkedListSetHead(newLinkedList, newLinkedListNode);
+    
+    return newLinkedList;
+    
+}
+
+void AOLinkedListSetHead(AOLinkedList *list, AOLinkedListNode *head) {
+    if (NULL != list && NULL != head) {
+        AOObjectRetain(head);
+        AOObjectRetain(list->_head);
+        
+         list->_head = head;
+    }
+}
+
+AOLinkedListNode *AOLinkedListGetHead(AOLinkedList *list) {
+    if (NULL != list) {
+        return list->_head;
+    }
+    return NULL;
+}
+
+void AOLinkedListSetCount(AOLinkedList *list, uint count) {
+    if (count == 0) {
+        AOLinkedListSetHead(list, NULL);
+    }
+    list->_count = count;
+    
+}
+
+uint AOLinkedListGetCount(AOLinkedList *list) {
+    if (NULL != list) {
+        return list->_count;
+    }
+    return 0;
+}
+
+void AOLinkedListAddObject(AOLinkedList *list, void *object) {
+    if (NULL != list && NULL != object) {
+        
+        AOLinkedListNode *newLinkedListNodeWithObject = AOLinkedListNodeCreateWithData(object);
+        AOLinkedListNode *head = AOLinkedListGetHead(list);
+        AOLinkedListNodeSetNextNode(newLinkedListNodeWithObject, head);
+        AOLinkedListSetHead(list, newLinkedListNodeWithObject);
+        AOLinkedListSetCount(list, (AOLinkedListGetCount(list) + 1));
+                             
+    }
+}
+
+void AOLinkedListRemoveFirstObject(AOLinkedList *list) {
+    if (NULL != list) {
+        
+        AOLinkedListNode *head = AOLinkedListGetHead(list);
+        AOLinkedListNode *nextNode = AOLinkedListNodeGetNextNode(head);
+        AOLinkedListSetHead(list, nextNode);
+        AOLinkedListSetCount(list, (AOLinkedListGetCount(list) - 1));
+        
+    }
+}
+
+void AOLinkedListRemoveAllObjects(AOLinkedList *list) {
+    if (NULL != list) {
+        
+        AOLinkedListSetCount(list, 0);
+    }
+}
+
+
+AOObject *AOLinkedListGetObjectBeforeObject(AOLinkedList *list, void *object, AOLinkedListSearchError *error) {
+    if (NULL != list && NULL != object) {
+        
+        AOLinkedListNode *head = AOLinkedListGetHead(list);
+        if (NULL == head) {
+            *error = AOLinkedListErrorLinkedListIsEmpty;
+            
+            return NULL;
+        }
+        
+        AOObject *currentObject = NULL;
+        AOObject *previousObject;
+        int count = 0;
+        while (NULL != head) {
+            previousObject = currentObject;
+            currentObject = AOLinkedListNodeGetData(head);
+            
+            if (currentObject == object) {
+                if (count == 0) {
+                    *error = AOLinkedListSearchErrorObjectIsHead;
+                }
+                else {
+                    *error = AOLinkedListSearchErrorNoError;
+                }
+                
+                return previousObject;
+            }
+            count++;
+            head = AOLinkedListNodeGetNextNode(head);
+        }
+    }
+    
+    *error = AOLinkedListSearchErrorObjectNotFound;
+    
+    return NULL;
+}
+
+AOObject *AOLinkedListGetObjectAfterObject(AOLinkedList *list, void *object, AOLinkedListSearchError *error) {
+    if (NULL != list && NULL != object) {
+        
+        AOLinkedListNode *head = AOLinkedListGetHead(list);
+        if (NULL == head) {
+            *error = AOLinkedListErrorLinkedListIsEmpty;
+            
+            return NULL;
+        }
+        AOObject *nextObject = NULL;
+        AOObject *currentObject;
+        int count = 0;
+        while (NULL != head) {
+            currentObject  = nextObject;
+            nextObject = AOLinkedListNodeGetData(head);
+            if (currentObject == object) {
+                if (count == 0) {
+                    *error = AOLinkedListSearchErrorObjectIsHead;
+                }
+                
+                else {
+                    *error = AOLinkedListSearchErrorNoError;
+                }
+                
+                return nextObject;
+            }
+            count++;
+            head = AOLinkedListNodeGetNextNode(head);
+        }
+    }
+    
+    *error = AOLinkedListSearchErrorObjectNotFound;
+    
+    return NULL;
+}
+
+bool AOLinkedListContainsObject(AOLinkedList *list, void *object) {
+    if (NULL != list && NULL != object) {
+      AOLinkedListNode *head = AOLinkedListGetHead(list);
+        if (NULL == head){
+            return false;
+        }
+        AOObject *currentObject;
+        while (NULL != head) {
+            currentObject = AOLinkedListNodeGetData(head);
+            if (currentObject == object) {
+                
+                return true;
+            }
+            head = AOLinkedListNodeGetNextNode(head);
+        }
+    }
+    return false;
+}
+
+bool AOLinkedListIsNotEmpty(AOLinkedList *list) {
+    return (NULL != list) && (NULL != AOLinkedListGetHead(list));
+}
+
--- AOHomeTask5/AOHomeTask5/Source/AOLinkedList.h
+++ AOHomeTask5/AOHomeTask5/Source/AOLinkedList.h
@@ -10,5 +10,49 @@
 #define __AOHomeTask5__AOLinkedList__
 
 #include <stdio.h>
+#include <stdbool.h>
+#include "AOObject.h"
+#include "AOLinkedListNode.h"
+
+typedef enum {
+    AOLinkedListSearchErrorNoError = 0,
+    AOLinkedListSearchErrorObjectIsHead,
+    AOLinkedListSearchErrorObjectNotFound,
+    AOLinkedListErrorLinkedListIsEmpty
+    
+} AOLinkedListSearchError;
+
+
+typedef struct {
+    AOObject _super;
+    
+    AOLinkedListNode *_head;
+    uint _count;
+    
+} AOLinkedList;
+
+void _AOLinkedListDeallocate(AOObject *object);
+
+AOLinkedList *AOLinkedListCreateWithObject(void *object);
+
+void AOLinkedListSetCount(AOLinkedList *list, uint count);
+
+uint AOLinkedListGetCount(AOLinkedList *list);
+
+AOObject *AOLinkedListGetObjectBeforeObject(AOLinkedList *list, void *object, AOLinkedListSearchError *error);
+
+AOObject *AOLinkedListGetObjectAfterObject(AOLinkedList *list, void *object, AOLinkedListSearchError *error);
+
+void AOLinkedListAddObject(AOLinkedList *list, void *object);
+
+void AOLinkedListRemoveFirstObject(AOLinkedList *list);
+
+void AOLinkedListRemoveAllObjects(AOLinkedList *list);
+
+bool AOLinkedListIsEmpty(AOLinkedList *list);
+
+bool AOLinkedListContainsObject(AOLinkedList *list, void *object);
+
+
 
 #endif /* defined(__AOHomeTask5__AOLinkedList__) */
