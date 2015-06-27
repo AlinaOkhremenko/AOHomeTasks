c0cb67fe9f4090cc7a319dff97698109441d57ef
Alina Okhremenko <slykaramba@gmail.com>
2015-06-27 18:14:56 +0300

    deleted and added new files

---
+++ AOHomeTask5/AOHomeTask5/Source/AOEnumerator.h
@@ -0,0 +1,14 @@
+//
+//  AOEnumerator.h
+//  AOHomeTask5
+//
+//  Created by Alina Okhremenko on 27.06.15.
+//  Copyright (c) 2015 Alina Okhremenko. All rights reserved.
+//
+
+#ifndef __AOHomeTask5__AOEnumerator__
+#define __AOHomeTask5__AOEnumerator__
+
+#include <stdio.h>
+
+#endif /* defined(__AOHomeTask5__AOEnumerator__) */
