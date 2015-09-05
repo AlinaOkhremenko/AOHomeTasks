89f2cfb79fb4fbb0737d9a4a8cadcf01f0c69c1f
Alina Okhremenko <slykaramba@gmail.com>
2015-09-05 22:44:02 +0300

    refactoring files

---
+++ ObjectiveC/ObjectiveC/AOEmployeeStateDelegateProtocol.h
@@ -0,0 +1,13 @@
+//
+//  AOEmployeeStateDelegateProtocol.h
+//  ObjectiveC
+//
+//  Created by Alina Okhremenko on 01.09.15.
+//  Copyright (c) 2015 Alina Okhremenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol AOEmployeeStateDelegateProtocol <NSObject>
+
+@end
